20159fc0df35d545d79c5bd21f5f7c5344404431
Marcin Michalik <terragady@gmail.com>
2021-04-26 16:51:13 +0200

    hell

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,51 @@
 #include <Arduino.h>
-#include <M2M_LM75A.h>
-M2M_LM75A lm75a;
+#include <Wire.h> //include Wire.h library
 
-
-void setup() {
-
-  pinMode(13, OUTPUT);
-  lm75a.begin();
-      digitalWrite(13, HIGH);
-
-  
-  delay(1000);
-  Serial.begin(9600);
-  delay(1000);
-
+void setup()
+{
+  Wire.begin(); // Wire communication begin
+  Serial.begin(9600); // The baudrate of Serial monitor is set in 9600
+  while (!Serial); // Waiting for Serial Monitor
+  Serial.println("\nI2C Scanner");
 }
 
-void loop() {
+void loop()
+{
+  byte error, address; //variable for error and I2C address
+  int nDevices;
 
+  Serial.println("Scanning...");
 
-  delay(1000);
-    digitalWrite(13, LOW);
-    
-  delay(1000);
-    digitalWrite(13, HIGH);
+  nDevices = 0;
+  for (address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
 
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println("  !");
+      nDevices++;
+    }
+    else if (error == 4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
 
-  delay(3000);
-pinMode(4, OUTPUT);
-    digitalWrite(4, HIGH);
-
+  delay(2000); // wait 5 seconds for the next I2C scan
 }
